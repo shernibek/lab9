8e1cbb9f06d14f225ed834484b79b99e2a05222d
shernibek <sherniyaz.tolganbayev@nu.edu.kz>
2022-10-22 23:52:15 +0600

    lab9 submission

---
+++ main.js
@@ -0,0 +1,30 @@
+function Perimeter (x, y) {
+    console.log(2 * (x + y));
+}
+
+Perimeter(3, 4.5);
+
+
+function areEqual(x, y) {
+
+if ((x - y) == 0) {
+           return true;
+}
+        else {
+            return false;
+}
+}
+console.log(areEqual(3,7));
+console.log(areEqual(55,6));
+console.log(areEqual(23,23));
+
+function between0and1 (x) {
+  if ((x >= 0  && x <= 1))
+     {
+    return true;
+     }
+    else
+     {
+    return false;
+  }
+}
